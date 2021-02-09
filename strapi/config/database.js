be3bfd012eef6564cdfe968b2837f9c6789ed615
Bruno Freitas <brunosfreitas@gmail.com>
2021-02-09 16:18:58 -0300

    changing connections

--- strapi/config/database.js
+++ strapi/config/database.js
@@ -4,15 +4,9 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'mongoose',
       settings: {
-        host: env('DATABASE_HOST', 'cluster0.znzd7.mongodb.net'),
-        srv: env.bool('DATABASE_SRV', true),
-        port: env.int('DATABASE_PORT', 27017),
-        database: env('DATABASE_NAME', 'strapi'),
-        username: env('DATABASE_USERNAME', 'strapi'),
-        password: env('DATABASE_PASSWORD', 'KI!2h8y6DitxRJNH'),
+        uri: env('DATABASE_URI'),
       },
       options: {
-        authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
         ssl: env.bool('DATABASE_SSL', true),
       },
     },
