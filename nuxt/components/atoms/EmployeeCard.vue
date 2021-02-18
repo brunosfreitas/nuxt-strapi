5941be311429d3fb679cfd9465c52846d0cd9ef5
Geovane Santana (Valtech) <geovane.santana@valtech.com>
2021-02-18 18:08:50 -0300

    new comp employee card

---
+++ nuxt/components/atoms/EmployeeCard.vue
@@ -0,0 +1,59 @@
+<template>
+    <div class="employee-card">
+        <div class="employee-card__item">
+            <img class="employee-card__illustration" :src="strapiSrc(imgSrc)" :alt="imgAlt">
+            <p class="employee-card__name">{{ name }}</p>
+            <p class="employee-card__position">{{ position }}</p>
+
+            <ul class="employee-card__social-links">
+                <li v-for="item in socialMediaLinks" :key="item.id">
+                    <a :href="item.url">
+                        <img :src="strapiSrc(item.icon.url)" :alt="item.icon.alternativeText">
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<script>
+import { getStrapiMedia } from '../../utils/medias.js';
+
+export default {
+    name: "EmployeeCard",
+    props: {
+        name: {
+            type: String,
+            default: ""
+        },
+        position: {
+            type: String,
+            default: ""
+        },
+        imgAlt: {
+            type: String,
+            default: ""
+        },
+        imgSrc: {
+            type: String,
+            default: ""
+        },
+        iconLink: {
+            type: String,
+            default: ""
+        },
+        socialMediaLinks: {
+            type: Array
+        }
+    },
+    methods: {
+        strapiSrc: function(img) {
+            return getStrapiMedia(img);
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
