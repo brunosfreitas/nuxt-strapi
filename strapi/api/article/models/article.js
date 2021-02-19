c8a0c4915f6bc285010afc3d7a675786efc4ebfd
Geovane Santana (Valtech) <geovane.santana@valtech.com>
2021-02-19 12:50:46 -0300

    automatically generating slugs

--- strapi/api/article/models/article.js
+++ strapi/api/article/models/article.js
@@ -5,4 +5,19 @@
  * to customize this model
  */
 
-module.exports = {};
+const slugify = require('slugify');
+
+module.exports = {
+  lifecycles: {
+    beforeCreate: async (data) => {
+      if (data.title) {
+        data.slug = slugify(data.title, { lower: true, strict: true });
+      }
+    },
+    beforeUpdate: async (params, data) => {
+      if (data.title) {
+        data.slug = slugify(data.title, { lower: true, strict: true });
+      }
+    }
+  }
+};
