be0260018e086bed578bed1d247174b9db709d21
Bruno Freitas <brunosfreitas@gmail.com>
2021-03-28 23:47:31 -0300

    Using recommended way to import images from assets folder

--- nuxt/components/atoms/Logo.vue
+++ nuxt/components/atoms/Logo.vue
@@ -1,6 +1,6 @@
 <template>
   <NuxtLink to="/">
-    <img class="NuxtLogo" src="@/assets/img/logo.png" alt="Logo">
+    <img class="NuxtLogo" src="~/assets/img/logo.png" alt="Logo">
   </NuxtLink>
 </template>
 
